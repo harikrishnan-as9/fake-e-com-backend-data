9c5a7b9c8d908fb743bf83fc132a43dd9371629d
Hari <harikrishnan.annappilly@gmail.com>
2023-02-02 11:55:41 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    'description_length_min': 1,
+    'description_length_max': 5,
+    "price_min": 10,
+    "price_max": 100,
+    "stock_min": 0,
+    "stock_max": 10,
+    "rating_min": 1,
+    "rating_max": 5,
+    "rated_by_min": 1,   
+    "rated_by_max": 10,   
+}
--- populate.py
+++ populate.py
@@ -2,32 +2,18 @@ import os
 import random
 from models.category import CategoryModel
 from models.product import ProductModel
-
-# def populate():
-#     electronics = CategoryModel(name='Electronics')
-#     toys = CategoryModel(name='Toys')
-
-#     electronics.save()
-#     toys.save()
-
-#     camera = ProductModel(name='camera'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=electronics)
-#     laptop = ProductModel(name='laptop'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=electronics)
-#     watch = ProductModel(name='watch'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=electronics)
-
-#     gun = ProductModel(name='gun'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=toys)
-#     waterbaloon = ProductModel(name='waterbaloon'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=toys)
-#     clay = ProductModel(name='clay'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=toys)
-#     lego = ProductModel(name='lego'.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=toys)
-
-#     camera.save()
-#     laptop.save()
-#     watch.save()
-#     gun.save()
-#     waterbaloon.save()
-#     clay.save()
-#     lego.save()
+from faker import Faker
+fake = Faker()
+from config import config
     
-
+def get_des(num=3):
+    text = ''
+    while num>0:
+        num = num-1
+        text += fake.text()
+    text = text.replace('\r\n','')
+    text = text.replace('\n','')
+    return text
 
 def populate():
     folder = 'items/'
@@ -41,5 +27,16 @@ def populate():
         Lines = file1.readlines()
         for line in Lines:
             product_name = line.strip()
-            product = ProductModel(name=product_name.capitalize(),price=random.randint(1, 10), stock=random.randint(0, 10),description='',rating=round(random.uniform(1,5),2),rated_by=random.randint(1, 100),category=category)
+            des = get_des(
+                random.randint(config['description_length_min'],config['description_length_min'])
+                )
+            product = ProductModel(
+                name=product_name.capitalize(),
+                price=random.randint(config['price_min'], config['price_max']), 
+                stock=random.randint(config['stock_min'], config['stock_max']),
+                description=des,
+                rating=round(random.uniform(config['rating_min'], config['rating_max']),2),
+                rated_by=random.randint(config['rated_by_min'], config['rated_by_max']),
+                category=category
+            )
             product.save()
